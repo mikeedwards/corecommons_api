5116b4a29d138040d56c099c7369cc6c0a41e69f
Mike Edwards <mike@onearmedman.com>
2012-10-07 19:43:36 -0400

    adding gunicorn

--- corecommons_api/settings.py
+++ corecommons_api/settings.py
@@ -108,10 +108,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'gunicorn',
 )
 
 # A sample logging configuration. The only tangible logging
